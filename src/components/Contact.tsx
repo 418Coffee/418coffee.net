20be94dbe0c20e4b29311a64951817abbef40035
418Coffee <matthijskes@gmail.com>
2023-06-02 22:05:25 +0200

    feat: contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,45 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Link, Text } from "@chakra-ui/react";
 
-const Contact = () => <Box>Contact</Box>;
+const Contact = () => (
+  <Box p="5">
+    <Heading as="h1" mb="5">
+      Contact
+    </Heading>
+    <Text>
+      If you would like to get in touch with me, you can reach me through the
+      following channels:
+      <br />
+      <br />
+      Discord: Arcturus#3828 <br />
+      Twitter:{" "}
+      <Link color="purple.500" href="https://twitter.com/kmattha" isExternal>
+        @kmattha
+      </Link>{" "}
+      or{" "}
+      <Link
+        color="purple.500"
+        href="https://twitter.com/messages/compose?recipient_id=2680090438"
+        isExternal
+      >
+        write me a DM
+      </Link>
+      <br />
+      Email: matthijskes <i>[at]</i> gmail <i>[dot]</i> com <br />
+      <br />
+      Feel free to connect with me on any of these platforms. I look forward to
+      hearing from you!
+      <br />
+      <br />
+      The source code for this website is available at{" "}
+      <Link
+        color="purple.500"
+        href="https://github.com/418Coffee/418coffee.net"
+      >
+        418coffee/418coffee.net
+      </Link>
+      .
+    </Text>
+  </Box>
+);
 
 export default Contact;
